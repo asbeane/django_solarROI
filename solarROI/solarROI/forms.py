28d070ea859d6e75b23ef872de3be6dd14d06e16
Andrew <asbeane@gmail.com>
2016-01-03 22:35:24 -0500

    Update forms.py

--- solarROI/solarROI/forms.py
+++ solarROI/solarROI/forms.py
@@ -2,28 +2,6 @@ from django import forms
 from django.core import validators
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-'''
-class CustomRegistrationFormTest(UserCreationForm):
-    def __init__(self):
-        self.fields = (
-            forms.EmailField(required=True),
-        )
-    
-    def isValidUsername(self, field_data, all_data):
-        try:
-            User.objects.get(username=field_data)
-        except User.DoesNotExist:
-            return
-        raise validators.ValidationError('The username "%s" is already taken.' % field_data)
-    
-    def save(self, new_data):
-        u = User.objects.create_user(new_data['username'],
-                                     new_data['email'],
-                                     new_data['password1'])
-        u.is_active = False
-        u.save()
-        return u
-'''
 
 class CustomRegistrationForm(UserCreationForm):
     email = forms.EmailField(required=True,\
